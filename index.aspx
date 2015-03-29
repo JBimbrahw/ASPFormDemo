5932181807621702104406e19c84043e700ed27a
supremesun <justin.bimbrahw@gmail.com>
2015-03-29 10:10:29 -0400

    changes to output required and javascript to be deleted

---
+++ index.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Project1.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="MyStyles.css" rel="stylesheet" />
+
+    <title>EZ Pay Cheque Creator</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h1>EZ Pay Cheque Creator</h1>
+    </div>
+        <asp:Label ID="lblFirstName" runat="server" Text="First Name"></asp:Label>
+        <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="lblLastName" runat="server" Text="Last Name"></asp:Label>
+        <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="lblNumHoursWorked" runat="server" Text="Number of Hours Worked"></asp:Label>
+        <asp:TextBox ID="txtNumHoursWorked" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="lblHourlyRate" runat="server" Text="Hourly Rate of Pay"></asp:Label>
+        <asp:TextBox ID="txtHourlyRate" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnGetPayCheque" runat="server" Text="Generate My Paycheque" OnClick="btnGetPayCheque_Click" OnClientClick ="showDiv()"/>
+        <br />
+        <br />
+        <asp:Button ID="btnClear" runat="server" OnClick="btnClear_Click" Text="Clear" />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div id ="hiddendiv">
+            <h2>Your Pay Cheque for the Month</h2>
+            <div class = "on_left">
+                <p>Employee:</p>
+                <p>Total Number of Hours Worked:</p>
+                <p>Number of OT Hours Worked:</p>
+                <p>Value of Regular Pay:</p>
+                <p>Value of Overtime Pay:</p>
+                <p>Hourly Pay:</p>
+                <p>Gross Pay:</p>
+                <p>Tax Deducted:</p>
+                <p>Net Pay:</p>
+            </div>
+            <asp:TextBox ID="txtOutput" runat="server" Visible="False" TextMode="MultiLine" Height="376px" Width="516px" style="margin-left: 36px; margin-top: 25px"></asp:TextBox>
+
+
+        </div>
+        <br />
+        <br />
+    </form>
+
+    <script type="text/javascript">
+        function showDiv() {
+            document.getElementById('hiddendiv').style.visibility = "visible";
+        }
+     </script>
+
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project1
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                SetFocus(txtFirstName);
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFirstName.Text = string.Empty;
+            txtLastName.Text = string.Empty;
+            txtNumHoursWorked.Text = string.Empty;
+            txtHourlyRate.Text = string.Empty;
+        }
+
+        protected void btnGetPayCheque_Click(object sender, EventArgs e)
+        {
+            
+            //Declare and Initialize Variables with Data from the Form
+            string empFirstName = txtFirstName.Text;
+            string empLastName = txtLastName.Text;
+            string numHours = txtNumHoursWorked.Text;
+            string hourlyRate = txtHourlyRate.Text;
+            string output = string.Empty;
+            
+            decimal dNumHours = Convert.ToDecimal(numHours);
+            decimal dHourlyRate = Convert.ToDecimal(hourlyRate);
+            
+            //Calculate the Gross Pay, Tax to be Paid, Net Pay
+            decimal taxRate = 0.24m;
+            decimal hoursBeforeOT = 40;
+            decimal dOTMultiplier = 1.5m;
+            decimal dOTRate = dOTMultiplier * dHourlyRate;
+            decimal dOTHours = 0;
+            decimal dRegularPay = 0;
+            decimal dOTPay = 0;
+            decimal dGrossPay = 0;
+            decimal dTax = 0;
+            decimal dNetPay = 0;
+
+
+            if (dNumHours <= hoursBeforeOT)
+            {
+                dGrossPay = dNumHours * dHourlyRate;
+            }
+            else
+            {
+                dOTHours = dNumHours - hoursBeforeOT;
+                dRegularPay = hoursBeforeOT * dHourlyRate;
+                dOTPay = dOTHours * dOTRate;
+                dGrossPay = dRegularPay + dOTPay;
+            }
+
+                       
+            dTax = dGrossPay * taxRate;
+            dNetPay = dGrossPay - dTax;
+
+            //Generate and Present the Pay Cheque
+            output = "            Employee:      " + empFirstName + " " + empLastName;
+            output += Environment.NewLine;
+            output += "Total Number of Hours Worked:   " + dNumHours.ToString();
+            output += Environment.NewLine;
+            output += "Number of OT Hours Worked:   " + dOTHours.ToString();
+            output += Environment.NewLine;
+            output += "Value of Regular Pay:   " + dRegularPay.ToString();
+            output += Environment.NewLine;
+            output += "Value of Overtime Pay:   " + dOTPay.ToString();
+            output += "         Hourly Pay:      " + dHourlyRate.ToString();
+            output += Environment.NewLine;
+            output += "          Gross Pay:      " + dGrossPay.ToString();
+            output += Environment.NewLine;
+            output += "       Tax Deducted:      " + dTax.ToString();
+            output += Environment.NewLine;
+            output += "            Net Pay:      " + dNetPay.ToString();
+
+            txtOutput.Visible = true;
+
+            txtOutput.Text = output;
+
+        }
+    }
+}
